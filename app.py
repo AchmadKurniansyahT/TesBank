59592d833bbce55800db15e696fe934f4434e6e5
Achmad Kurniansyah Thalib <33804802+AchmadKurniansyahT@users.noreply.github.com>
2025-12-16 08:56:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import imblearn
-import imblearn.pipeline
 
 # ==================================================
 # PAGE CONFIG (HARUS PALING ATAS)
@@ -137,5 +135,6 @@ if st.button("🔍 Predict"):
         st.success(f"✅ **Subscribe**\n\nProbability: **{pred_prob:.2%}**")
     else:
         st.error(f"❌ **Not Subscribe**\n\nProbability: **{pred_prob:.2%}**")
+
 
 
